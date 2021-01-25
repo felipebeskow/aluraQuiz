b937a1f62c02be1b1dbf0834a69e88467bb0b4db
Felipe Beskow <felipebeskow@outlook.com>
2021-01-25 17:03:32 -0300

    Metadados otimizados

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,22 @@
 export default function Header(props) {
-    return <head>
-        <title>AluraQuiz! - Steven Universo</title>
-        <meta property="og:description" content="Um quiz sobre Steven Universo "/>
-        <meta property="og:image" content={props.children}/>
-        <meta property="og:image:type" content="image/jpeg"/>
-        <meta property="og:image:width" content="800"/>
-        <meta property="og:image:height" content="600"/>
-    </head>
+    return (
+        <head>
+            
+            <title>AluraQuiz! - Steven Universo</title>
+            <meta property="og:description" content="Um quiz sobre Steven Universo "/>
+
+            <meta property="og:url" content="https://aluraquiz.felipebeskow.vercel.app/"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:title" content="AluraQuiz! - Steven Universo"/>
+            <meta property="og:description" content="Um quiz sobre Steven Universo "/>
+            <meta property="og:image" content="https://hipertextual.com/files/2020/03/hipertextual-2020465354.jpeg"/>
+
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:domain" content="aluraquiz.felipebeskow.vercel.app"/>
+            <meta property="twitter:url" content="https://aluraquiz.felipebeskow.vercel.app/"/>
+            <meta name="twitter:title" content="AluraQuiz! - Steven Universo"/>
+            <meta name="twitter:description" content="Um quiz sobre Steven Universo "/>
+            <meta name="twitter:image" content="https://hipertextual.com/files/2020/03/hipertextual-2020465354.jpeg"/>
+        </head>
+    );
 }
